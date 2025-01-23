478e373bfe9aeb59c082daf1c5ad2c82e2a6e052
Sergio <sergio11iw@mail.ru>
2025-01-23 23:40:55 +0500

    Add files via upload

--- core/urls.py
+++ core/urls.py
@@ -3,8 +3,9 @@ from django.contrib import admin
 from django.urls import path
 from .import views
 urlpatterns = [
-    path('', views.main, name='main'),
+    path('', views.main),
     path('posts/<int:post_id>', views.post_detail, name='post_detail'),
+    path('posts/add', views.post_add, name='post_add'),
 ]
 
 
--- core/views.py
+++ core/views.py
@@ -4,14 +4,16 @@ from django.http import HttpResponse
 from .models import Post
 def main(request):
     posts = Post.objects.all()
-    if request.method == 'POST':
-        title_data = request.POST['title']
-        text_data = request.POST['text']
-        if title_data and text_data:
-             Post.objects.create(title=title_data, text=text_data)
     return render(request, 'main.html', {'posts': posts})
 def post_detail(request, post_id):
     post = Post.objects.get(id=post_id)
     return render(request, 'post_detail.html', {'post': post})
-
-# Create your views here.
+def post_add(request):
+    if request.method == 'POST':
+        title_data = request.POST.get('title')
+        text_data = request.POST.get('text')
+        print(title_data)
+        print(text_data)
+        if text_data and title_data:
+            Post.objects.create(text=text_data, title=title_data)
+    return render(request, 'post_add.html', {'post_add': post_add})
